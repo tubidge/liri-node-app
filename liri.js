533b12f64246d3ddbb154e05462c9d56ba09163e
tubidge <tubidge@gmail.com>
2019-04-14 14:30:20 -0600

    first api call

--- liri.js
+++ liri.js
@@ -1,5 +1,48 @@
 require("dotenv").config();
+var inqr = require("inquirer");
+var axios = require("axios");
+var moment = require("moment");
 
 var keys = require("./keys.js");
 
-var spotify = new spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+console.log(moment().format('dddd'));
+
+inqr.prompt([{
+    name: "question",
+    message: "What can I help you with? Type: concert-this, spotify-this-song, movie-this, or do-what-it-says.",
+}]).then(function (resp) {
+    if (resp.question === "concert-this") {
+        concertSearch();
+    } else if (resp.question === "spotify-this-song") {
+        songSearch();
+    } else if (resp.question === "movie-this") {
+        movieSearch();
+    } else if (resp.question === "do-what-it-says") {
+        doWhatItSays();
+    };
+});
+
+function concertSearch() {
+    inqr.prompt([{
+        name: "artist",
+        message: "What artist/band?"
+    }]).then(function (resp) {
+        var artist = resp.artist;
+        var url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+        axios.get(url).then(
+            function (resp) {
+                console.log(url)
+                console.log(resp.data.length);
+                console.log(`Venue: ${resp.data[0].venue.name}`);
+                console.log(`Location: ${resp.data[0].venue.city}, ${resp.data[0].venue.region}, ${resp.data[0].venue.country}`);
+                var date = resp.data[0].datetime;
+                console.log(date);
+
+
+            }
+        )
+
+    })
+};
