b467f0bf6ea14f9181dd7d3420d537c05ec2158a
tubidge <tubidge@gmail.com>
2019-04-15 20:00:23 -0600

    all 4 working

--- liri.js
+++ liri.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 var inqr = require("inquirer");
 var axios = require("axios");
+var Spotify = require("node-spotify-api");
 var moment = require("moment");
+var fs = require("fs");
 
 var keys = require("./keys.js");
 
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
-console.log(moment().format('dddd'));
 
 inqr.prompt([{
     name: "question",
@@ -35,14 +36,111 @@ function concertSearch() {
             function (resp) {
                 console.log(url)
                 console.log(resp.data.length);
-                console.log(`Venue: ${resp.data[0].venue.name}`);
-                console.log(`Location: ${resp.data[0].venue.city}, ${resp.data[0].venue.region}, ${resp.data[0].venue.country}`);
-                var date = resp.data[0].datetime;
-                console.log(date);
-
-
+                for (var i = 0; i < resp.data.length; i++) {
+                    console.log(`\n------ Concert # ${i} ------`)
+                    console.log(`Venue: ${resp.data[i].venue.name}`);
+                    console.log(`Location: ${resp.data[i].venue.city}, ${resp.data[i].venue.region}, ${resp.data[i].venue.country}`);
+                    var date = resp.data[i].datetime;
+                    console.log(`Date: ${date}`);
+                }
             }
         )
+    })
+};
 
+function songSearch() {
+    inqr.prompt([{
+        name: "song",
+        message: "What song?"
+    }]).then(function (resp) {
+        spotify.search({
+            type: "track",
+            query: resp.song,
+            limit: 1
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            if (data.tracks.items[0]) {
+                console.log(`Artist: ${data.tracks.items[0].album.artists[0].name}`);
+                console.log(`Song Name: ${data.tracks.items[0].name}`)
+                console.log(`Song Link: ${data.tracks.items[0].external_urls.spotify}`)
+                console.log(`Album: ${data.tracks.items[0].album.name}`)
+            } else {
+                spotify.search({
+                    type: "track",
+                    query: "the sign",
+                }, function (err, data) {
+                    if (err) {
+                        return console.log('Error occured: ' + err)
+                    }
+                    console.log(`Artist: ${data.tracks.items[8].album.artists[0].name}`);
+                    console.log(`Song Name: ${data.tracks.items[8].name}`)
+                    console.log(`Song Link: ${data.tracks.items[8].external_urls.spotify}`)
+                    console.log(`Album: ${data.tracks.items[8].album.name}`)
+                })
+            }
+        });
+    })
+};
+
+function movieSearch() {
+    inqr.prompt([{
+        name: "movie",
+        message: "What movie?"
+    }]).then(function (resp) {
+        var url = `http://www.omdbapi.com/?t=${resp.movie}&plot=short&apikey=trilogy`
+        axios.get(url).then(
+            function (resp) {
+                if (resp.data["Title"]) {
+                    console.log(url);
+                    console.log(`Title: ${resp.data["Title"]}`);
+                    console.log(`Release Year: ${resp.data["Year"]}`);
+                    console.log(`IMDB Rating: ${resp.data["Ratings"][0]["Value"]}`);
+                    console.log(`Rotten Tomatoes Rating: ${resp.data["Ratings"][1]["Value"]}`);
+                    console.log(`Produced In: ${resp.data["Country"]}`);
+                    console.log(`Language: ${resp.data["Language"]}`);
+                    console.log(`Plot: ${resp.data["Plot"]}`);
+                    console.log(`Cast: ${resp.data["Actors"]}`);
+                } else {
+                    axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&plot=short&apikey=trilogy").then(
+                        function (resp) {
+                            console.log(url);
+                            console.log(`Title: ${resp.data["Title"]}`);
+                            console.log(`Release Year: ${resp.data["Year"]}`);
+                            console.log(`IMDB Rating: ${resp.data["Ratings"][0]["Value"]}`);
+                            console.log(`Rotten Tomatoes Rating: ${resp.data["Ratings"][1]["Value"]}`);
+                            console.log(`Produced In: ${resp.data["Country"]}`);
+                            console.log(`Language: ${resp.data["Language"]}`);
+                            console.log(`Plot: ${resp.data["Plot"]}`);
+                            console.log(`Cast: ${resp.data["Actors"]}`);
+                        }
+                    )
+                }
+            }
+        )
+    })
+};
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err)
+        }
+        var output = data.split(",");
+        console.log(output[0]);
+        spotify.search({
+            type: "track",
+            query: output[1],
+            limit: 1
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(`Artist: ${data.tracks.items[0].album.artists[0].name}`);
+            console.log(`Song Name: ${data.tracks.items[0].name}`)
+            console.log(`Song Link: ${data.tracks.items[0].external_urls.spotify}`)
+            console.log(`Album: ${data.tracks.items[0].album.name}`)
+        })
     })
 };
